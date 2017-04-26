4b628d7c385a29635cc496180bd9946cedbe95e3
sammy9494 <salazar.sammy@hotmail.com>
2017-04-25 19:17:50 -0600

    partesitas chiquitas

---
+++ crm/crm/frm_buscarfct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cobros
+{
+    public partial class frm_buscarfct : Form
+    {
+        public frm_buscarfct()
+        {
+            InitializeComponent();
+        }
+        public crm.entidades.factura facturaselec { get; set;}
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = crm.datos.Buscar(Convert.ToInt32(textBox1.Text));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id_factura = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                facturaselec = crm.datos.ObtenerFactura(id_factura);
+                //MessageBox.Show();
+
+                this.Close();
+            }
+            else
+                MessageBox.Show("debe de seleccionar una fila");
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
